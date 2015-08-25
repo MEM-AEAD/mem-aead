859aac888a008a4e8af14442a579a6d5a9bc813d
Samuel Neves <sneves@dei.uc.pt>
2015-08-25 01:51:47 +0100

    alternative kdf experiment; not better

--- opp/ymm/storm.c
+++ opp/ymm/storm.c
@@ -16,13 +16,87 @@
 #include "v2.h"
 #include "v4.h"
 
-#define STORM_PAD(out, in, inlen) do { \
-  memset(out, 0, BYTES(STORM_B));      \
-  memcpy(out, in, inlen);              \
-  out[inlen] = 0x01;                   \
-} while(0)
+/* this is x86, so we can just memcpy */
+static uint64_t load64(const void * in) {
+  uint64_t x;
+  memcpy(&x, in, sizeof x);
+  return x;
+}
+
+static void storm_pad(unsigned char * out, const void * in, size_t inlen) {
+  memset(out, 0, BYTES(STORM_B));
+  memcpy(out, in, inlen);
+  out[inlen] = 0x01;
+}
 
 static void storm_kdf(uint64_t * Ka, uint64_t * Ke, const uint8_t * k, const uint8_t * n) {
+#if 0
+  __m256i B[8];
+  int i;
+
+  B[0] = _mm256_broadcastsi128_si256( LOADU128(n +  0) );
+  B[1] = _mm256_setzero_si256();
+  B[2] = _mm256_broadcastsi128_si256( LOADU128(k +  0) );
+  B[3] = _mm256_broadcastsi128_si256( LOADU128(k + 16) );
+  B[4] = _mm256_setzero_si256();
+  B[5] = _mm256_setzero_si256();
+  B[6] = _mm256_broadcastsi128_si256(_mm_set_epi64x(STORM_R, STORM_W));
+  B[7] = _mm256_set_epi64x(1, STORM_T, 0, STORM_T);
+
+#define G(A0,A1,B0,B1,C0,C1,D0,D1) do {     \
+  A0 = ADD256(A0, B0); A1 = ADD256(A1, B1); \
+  D0 = XOR256(D0, A0); D1 = XOR256(D1, A1); \
+  D0 = ROT256(D0, 32); D1 = ROT256(D1, 32); \
+  C0 = ADD256(C0, D0); C1 = ADD256(C1, D1); \
+  B0 = XOR256(B0, C0); B1 = XOR256(B1, C1); \
+  B0 = ROT256(B0, 24); B1 = ROT256(B1, 24); \
+                                            \
+  A0 = ADD256(A0, B0); A1 = ADD256(A1, B1); \
+  D0 = XOR256(D0, A0); D1 = XOR256(D1, A1); \
+  D0 = ROT256(D0, 16); D1 = ROT256(D1, 16); \
+  C0 = ADD256(C0, D0); C1 = ADD256(C1, D1); \
+  B0 = XOR256(B0, C0); B1 = XOR256(B1, C1); \
+  B0 = ROT256(B0, 63); B1 = ROT256(B1, 63); \
+} while(0)
+
+#define DIAGONALIZE(A0, A1, B0, B1, C0, C1, D0, D1) do { \
+    __m256i t0, t1;                                      \
+    t0 = _mm256_alignr_epi8(B1, B0, 8);                  \
+    t1 = _mm256_alignr_epi8(B0, B1, 8);                  \
+    B0 = t0; B1 = t1;                                    \
+                                                         \
+    t0 = C0; C0 = C1; C1 = t0;                           \
+                                                         \
+    t0 = _mm256_alignr_epi8(D1, D0, 8);                  \
+    t1 = _mm256_alignr_epi8(D0, D1, 8);                  \
+    D0 = t1; D1 = t0;                                    \
+} while(0)
+
+#define UNDIAGONALIZE(A0, A1, B0, B1, C0, C1, D0, D1) do { \
+    __m256i t0, t1;                                        \
+    t0 = _mm256_alignr_epi8(B0, B1, 8);                    \
+    t1 = _mm256_alignr_epi8(B1, B0, 8);                    \
+    B0 = t0; B1 = t1;                                      \
+                                                           \
+    t0 = C0; C0 = C1; C1 = t0;                             \
+                                                           \
+    t0 = _mm256_alignr_epi8(D0, D1, 8);                    \
+    t1 = _mm256_alignr_epi8(D1, D0, 8);                    \
+    D0 = t1; D1 = t0;                                      \
+} while(0)
+
+  for(i = 0; i < STORM_R; ++i) {
+    G(B[0],B[1],B[2],B[3],B[4],B[5],B[6],B[7]);
+    DIAGONALIZE(B[0],B[1],B[2],B[3],B[4],B[5],B[6],B[7]);
+    G(B[0],B[1],B[2],B[3],B[4],B[5],B[6],B[7]);
+    UNDIAGONALIZE(B[0],B[1],B[2],B[3],B[4],B[5],B[6],B[7]);
+  }
+
+  for(i = 0; i < 8; ++i) {
+    STOREU128(&Ka[2*i], _mm256_extracti128_si256(B[i], 0));
+    STOREU128(&Ke[2*i], _mm256_extracti128_si256(B[i], 1));
+  }
+#else
   __m128i B[16] = {0};
 
   B[ 0] = _mm_set1_epi64x(load64(n + 0));
@@ -54,6 +128,7 @@ static void storm_kdf(uint64_t * Ka, uint64_t * Ke, const uint8_t * k, const uin
   V2_UNPACK(2);
   V2_UNPACK(3);
 #undef V2_UNPACK
+#endif
 }
 
 static void storm_hash_data(__m256i T[4], const uint8_t * h, size_t hlen, uint64_t L[16+4]) {
@@ -87,7 +162,7 @@ static void storm_hash_data(__m256i T[4], const uint8_t * h, size_t hlen, uint64
     uint8_t lastblock[BYTES(STORM_B)];
     __m256i B[4];
     V1_MASK_UPDATE(L);
-    STORM_PAD(lastblock, h, hlen);
+    storm_pad(lastblock, h, hlen);
     V1_LOAD_BLOCK(B, lastblock);
     V1_BLOCKCIPHER_ROTATED_F(B, L, 256);
     V1_ACCUMULATE(T, B);
@@ -130,7 +205,7 @@ static void storm_encrypt_data(__m256i T[4], uint8_t * c, const uint8_t * m, siz
     __m256i B[4];
     int i;
     V1_MASK_UPDATE(L);
-    STORM_PAD(lastblock, m, mlen);
+    storm_pad(lastblock, m, mlen);
     V1_ZERO_BLOCK(B);
     V1_BLOCKCIPHER_ROTATED_F(B, L, 768);
     for(i = 0; i < 4; ++i) { /* lastblock xor B and T xor last block */
@@ -179,7 +254,7 @@ static void storm_decrypt_data(__m256i T[4], uint8_t * m, const uint8_t * c, siz
 
     V1_MASK_UPDATE(L);
 
-    STORM_PAD(lastblock, c, clen);
+    storm_pad(lastblock, c, clen);
     V1_ZERO_BLOCK(B);
     V1_BLOCKCIPHER_ROTATED_F(B, L, 768);
     for(i = 0; i < 4; ++i) { /* lastblock xor B */
@@ -187,7 +262,7 @@ static void storm_decrypt_data(__m256i T[4], uint8_t * m, const uint8_t * c, siz
       STOREU256(&lastblock[32 * i], XOR256(B[i], C_i));
     }
     memcpy(m, lastblock, clen);
-    STORM_PAD(lastblock, m, clen);
+    storm_pad(lastblock, m, clen);
     for(i = 0; i < 4; ++i) { /* T xor last block */
       T[i] = XOR256(T[i], LOADU256(&lastblock[32 * i]));
     }
